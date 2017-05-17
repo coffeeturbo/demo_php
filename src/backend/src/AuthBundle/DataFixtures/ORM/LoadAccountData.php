129c2bff759f638c4548f74da19cde048869412d
hck <art.hacker@ya.ru>
2017-05-17 15:51:57 +0300

    Tests....

--- src/backend/src/AuthBundle/DataFixtures/ORM/LoadAccountData.php
+++ src/backend/src/AuthBundle/DataFixtures/ORM/LoadAccountData.php
@@ -10,6 +10,12 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class LoadAccountData implements FixtureInterface, ContainerAwareInterface
 {
 
+    public $accountsData = [
+        ["email"=>"testuser1@domain.com","password"=>"4zFBLC", "roles"=>[]],
+        ["email"=>"testuser2@domain.com","password"=>"UjqjCB", "roles"=>[]],
+        ["email"=>"testuser3@domain.com","password"=>"GhuxMb", "roles"=>[]]
+    ];
+    
     /**
      * @var ContainerInterface
      */
@@ -21,22 +27,16 @@ class LoadAccountData implements FixtureInterface, ContainerAwareInterface
 
     public function load(ObjectManager $manager)
     {
-        $data = [
-            ["email"=>"testuser1@domain.com","password"=>"4zFBLC", "roles"=>[]],
-            ["email"=>"testuser2@domain.com","password"=>"UjqjCB", "roles"=>[]],
-            ["email"=>"testuser3@domain.com","password"=>"GhuxMb", "roles"=>[]]
-        ];
-
         /** @var UserManager $userManager */
         $userManager = $this->container->get('fos_user.user_manager');
 
-        foreach ($data as $row) {
+        foreach ($this->accountsData as $data) {
             $account = $userManager->createUser();
             $account->setEnabled(true);
-            $account->setPlainPassword($row['password']);
-            $account->setUsername($row['email']);
-            $account->setEmail($row['email']);
-            $account->setRoles($row['roles']);
+            $account->setPlainPassword($data['password']);
+            $account->setUsername($data['email']);
+            $account->setEmail($data['email']);
+            $account->setRoles($data['roles']);
             $userManager->updateUser($account);
         }
     }
--- src/backend/src/AuthBundle/Tests/SignInControllerTest.php
+++ src/backend/src/AuthBundle/Tests/SignInControllerTest.php
@@ -2,22 +2,43 @@
 
 namespace AuthBundle\Tests;
 
+use AuthBundle\DataFixtures\ORM\LoadAccountData;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SignInControllerTest extends WebTestCase
 {
+
+    /** @var \Doctrine\ORM\EntityManager */
+    private $em;
+    private $accountsData;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
+        
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+        
+//        $fixtures = new LoadAccountData();
+//        $fixtures->load($this->em);
+//        $this->accountsData = $fixtures->accountsData; 
+    }
+    
     public function testIndex()
     {
-        $client = static::createClient();
+       $client = static::createClient();
 
-        $crawler = $client->request('POST', '/api/auth/sign-in', [], [], ['content-type' => 'application/json'], json_encode(
-            [
-                "username"=>"admin",
-                "password"=>"lolipop",
-                "dont_remember"=>false
-            ]
+        $client->request('POST', '/auth/sign-in', [], [], ['content-type' => 'application/json'], json_encode(
+        [
+            "username"=>"testuser1@domain.com",
+            "password"=>"4zFBLC"
+        ]
         ));
-        
+        echo $client->getResponse()->getContent();die;
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
