dc11e4e23c024c740ab75c49b1a136cbe94a8e3f
hck <art.hacker@ya.ru>
2017-05-22 03:19:21 +0300

    Refactor

--- src/frontend/src/module/Translate/Service/LocaleService.ts
+++ src/frontend/src/module/Translate/Service/LocaleService.ts
@@ -4,12 +4,18 @@ import {Locale} from "../Entity/Definitions";
 @Injectable()
 export class LocaleService {
     private defaultTocale: Locale = require('../../../app/config.json').locale;
-
+    private locale: Locale;
+    
+    constructor() {
+        this.locale = <Locale>localStorage.getItem('locale') || this.defaultTocale;
+    }
+    
     public getLocale(): Locale {
-        return <Locale>localStorage.getItem('locale') || this.defaultTocale;
+        return this.locale;
     }
 
     public setLocale(locale: Locale): void {
+        this.locale = locale;
         localStorage.setItem('locale', locale);
     }
 }
