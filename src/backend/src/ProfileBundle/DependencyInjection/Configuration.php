15b2e4a35352eb8ea6cd515251108f0aba97dec8
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2017-05-29 12:40:41 +0300

    fix DI error

--- src/backend/src/ProfileBundle/DependencyInjection/Configuration.php
+++ src/backend/src/ProfileBundle/DependencyInjection/Configuration.php
@@ -9,6 +9,7 @@ class Configuration  implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('profile');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
