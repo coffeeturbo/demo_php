8833baf2f058aa6278a35e34b2e905fbb96274b5
hck <art.hacker@ya.ru>
2017-06-01 05:34:49 +0300

    add id to profile entity add cache to profile resolver add check if profile undefined

--- src/frontend/modules/Profile/Service/ProfileResolver.ts
+++ src/frontend/modules/Profile/Service/ProfileResolver.ts
@@ -8,11 +8,26 @@ import {ProfileGetResponse} from "../Http/Response/ProfileGetResponse";
 
 @Injectable()
 export class ProfileResolver implements Resolve<Profile> {
-
+    private profiles: Profile[] = [];
     constructor(private rest: ProfileRESTService) {}
     
     resolve(route: ActivatedRouteSnapshot): Observable<Profile | ResponseFailure> 
     {
-        return this.rest.get(route.params.id).map((profileGetResponse: ProfileGetResponse) => profileGetResponse.entity);
+        try {
+            return this.getFromCache(route.params.id);
+        } catch(e) {
+            return this.rest.get(route.params.id).map((profileGetResponse: ProfileGetResponse) => {
+                this.profiles.push(profileGetResponse.entity);
+                return profileGetResponse.entity
+            });
+        }
+    }
+    
+    private getFromCache(profileId: number) {
+        let profile: Profile = this.profiles.filter((profile)=>profile.id == profileId).shift();
+        if(!profile) {
+            throw new Error(`Profile with id ${profileId} is not cached`);
+        }
+        return Observable.of(profile);
     }
 }
