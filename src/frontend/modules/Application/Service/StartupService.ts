cd343e3850cb5fd20099b17b6fa8e268f6e6c079
hck <art.hacker@ya.ru>
2017-06-14 14:41:52 +0300

    add load info

--- src/frontend/modules/Application/Service/StartupService.ts
+++ src/frontend/modules/Application/Service/StartupService.ts
@@ -11,9 +11,11 @@ export class StartupService {
     constructor(private injector: Injector) {}
 
     init(): Promise<any> {
+        let status: HTMLElement = document.getElementById("statusText");
         let authService: AuthService = this.injector.get(AuthService);
 
         if (TokenRepository.isTokenExist() && TokenRepository.getTokenExpTime() < 0) { // If token expired wait before get a new
+            status.innerText = "Авторизация...";
             let onAuth = new EventEmitter<void>(); // Отдельный event, т.к. вызываем complete, завершающий его. а нам не нужно что бы основной event завершался
             authService.onAuth.subscribe(() => onAuth.complete());
             
