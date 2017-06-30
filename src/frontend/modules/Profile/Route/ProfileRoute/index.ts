74b58f75f9de3df2c392d71b9f0238eadd38a091
hck <art.hacker@ya.ru>
2017-06-30 04:41:13 +0300

    now use backend avatar or pallete avatar update profile entity

--- src/frontend/modules/Profile/Route/ProfileRoute/index.ts
+++ src/frontend/modules/Profile/Route/ProfileRoute/index.ts
@@ -1,23 +1,26 @@
-import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 
 import {Profile} from "../../Entity/Profile";
 import {CropperService} from "../../../Common/Cropper/Service/CropperService";
 import {AvatarUploadRequest} from "../../Http/Request/AvatarUploadRequest";
 import {ProfileService} from "../../Service/ProfileService";
+import {PalleteService} from "../../../Common/Pallete/Service/PalleteService";
 
 @Component({
     templateUrl: "./template.pug",
     styleUrls: ["./style.shadow.scss"]
 })
-export class ProfileRoute implements OnInit {
+export class ProfileRoute implements OnInit, AfterViewInit {
 
     public profile: Profile;
     public avatarPath: string = 'https://pbs.twimg.com/profile_images/378800000796578930/bb2119f37f717b0bc551923f7fdf3d9f_400x400.jpeg';
+    @ViewChild('crop') cropImage: ElementRef;
 
     constructor(
         private route: ActivatedRoute, 
         private profileService: ProfileService, 
+        public palleteService: PalleteService, 
         public cropperService: CropperService
     ) {}
 
@@ -25,10 +28,21 @@ export class ProfileRoute implements OnInit {
         this.profile = this.route.snapshot.data["profile"];
     }
     
-    @ViewChild('crop') cropImage: ElementRef;
-    
     ngAfterViewInit() {
         this.cropperService.init(this.cropImage.nativeElement);
+    }
+
+    public getProfileColor() {
+        return this.palleteService.encode(this.profile.name);
+    }
+    
+    public getProfileFirstLetters() {
+        return this.profile.name
+            .split(" ")
+            .slice(0,2)
+            .map(item => item.charAt(0).toUpperCase())
+            .join(" ")
+        ;
     }
     
     submit() {
@@ -42,9 +56,9 @@ export class ProfileRoute implements OnInit {
             image: cropperImage
         };
         
-        this.profileService.uploadAvatar(this.profile, avatarUploadRequest).subscribe((profile)=>{
-            console.log(profile);
-        })
+        this.profileService.uploadAvatar(this.profile, avatarUploadRequest)
+            .finally(() => this.cropperService.destroy())
+            .subscribe((profile) => this.profile = profile)
+        ;
     }
 }
-
