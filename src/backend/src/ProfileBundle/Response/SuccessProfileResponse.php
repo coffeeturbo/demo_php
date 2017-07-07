90e3e91622c81d6827d25c860f0077408afe904b
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2017-07-07 14:13:24 +0300

    fix backdrop error

--- src/backend/src/ProfileBundle/Response/SuccessProfileResponse.php
+++ src/backend/src/ProfileBundle/Response/SuccessProfileResponse.php
@@ -28,7 +28,7 @@ class SuccessProfileResponse extends JsonResponse implements \JsonSerializable
                 'name' => $profile->getName(),
                 'alias' => $profile->getAlias(),
                 'avatar' => $profile->getAvatarCollection()->jsonSerialize(),
-                'backdrop' => $profile->getBackdropCollection()->jsonSerialize(),
+                'backdrop' => $profile->getBackdrop(),
                 'birth_date' => $profile->getBirthDate() ? $profile->getBirthDate()->format(Profile::BIRTH_DATE_FORMAT) : null,
                 'verified' => $profile->isVerified(),
                 'created' => $profile->getCreated()->format(\DateTime::W3C)
