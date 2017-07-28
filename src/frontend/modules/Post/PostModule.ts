633614d31697e66d5cbe9a1e27748563f6b7b518
hck <art.hacker@ya.ru>
2017-07-28 17:33:05 +0300

    implements tags

--- src/frontend/modules/Post/PostModule.ts
+++ src/frontend/modules/Post/PostModule.ts
@@ -1,4 +1,8 @@
 import {NgModule} from "@angular/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {DragulaModule} from "ng2-dragula";
+import {TagInputModule} from "ngx-chips";
+
 import {PostRESTService} from "./Service/PostRESTService";
 import {PostComponent} from "./Component/Post/index";
 import {PostResolver} from "./Service/PostResolver";
@@ -6,11 +10,12 @@ import {PostTitleResolver} from "./Service/PostTitleResolver";
 import {PostRoute} from "./Route/PostRoute/index";
 import {AddPostRoute} from "./Route/AddPostRoute/index";
 import {CommonModule} from "../Common/CommonModule";
-import {DragulaModule} from "ng2-dragula";
 import {AttachmentModule} from "../Attachment/AttachmentModule";
 
 @NgModule({
     imports: [
+        TagInputModule,
+        BrowserAnimationsModule,
         DragulaModule,
         AttachmentModule,
         CommonModule
