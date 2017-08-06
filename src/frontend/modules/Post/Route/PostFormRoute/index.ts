a2d4fd379ff5e310528110056096becb20e7666c
hck <art.hacker@ya.ru>
2017-08-07 00:52:54 +0300

    add autocomplete tags update ngxchips add validation messages

--- src/frontend/modules/Post/Route/PostFormRoute/index.ts
+++ src/frontend/modules/Post/Route/PostFormRoute/index.ts
@@ -5,6 +5,7 @@ import {TranslationService} from "@angular-addons/translate";
 import {AttachmentType} from "../../../Attachment/Entity/Attachment";
 import {Tag} from "../../Entity/Tag";
 import {Post} from "../../Entity/Post";
+import {Observable} from "rxjs/Observable";
 
 @Component({
     templateUrl: './template.pug',
@@ -14,47 +15,56 @@ import {Post} from "../../Entity/Post";
 export class PostFormRoute implements OnInit {
 
     public attachments = new FormArray([]);
+    public submitted: boolean = false;
     public saved: boolean = true;
     public dragulaOptions = {
         moves: (el, container, handle) => handle.classList.contains('drag') || handle.parentElement.classList.contains('drag')
     };
 
-    public form: FormGroup = this.initForm();
+    public form: FormGroup = new FormGroup({
+        title: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+        tags: new FormControl(null, [Validators.required]),
+        attachments: this.attachments
+    });
+
     public authorTag: Tag = {
-        display: this.translationService.translate("Author's"),
-        value: this.translationService.translate("Author's")
+        display: this.translationService.translate("author's"),
+        value: this.translationService.translate("author's")
     };
-    
-    constructor(private translationService: TranslationService) {}
+
+    constructor(private translationService: TranslationService) {
+    }
 
     ngOnInit() {
+        try {
+            let post: Post = JSON.parse(localStorage.getItem("new-post"));
+            
+            if (post.attachments.length > 0) {
+                post.attachments.map(
+                    attachment => this.addAttachment(attachment.type, attachment.value)
+                );
+            }
+
+            if (post.title) {
+                this.form.controls.title.setValue(post.title);
+                this.form.controls.title.markAsDirty();
+            }
+            
+            if (post.tags) {
+                this.form.controls.tags.setValue(post.tags);
+                this.form.controls.tags.markAsDirty();
+            }
+        } catch (e) {}
+
         this.form.valueChanges
-            .debounceTime(300) // Ohyenable feature 
+            .debounceTime(500) // Ohyenable feature 
             .do(() => this.saved = false)
-            .debounceTime(700) // Ohyenable feature 
+            .debounceTime(500) // Ohyenable feature 
             .subscribe((post: Post) => {
                 localStorage.setItem("new-post", JSON.stringify(post));
                 this.saved = true;
             });
-    }
 
-    public initForm(): FormGroup {
-        let post: Post;
-        try {
-            post = JSON.parse(localStorage.getItem("new-post"));
-            post.attachments.map(
-                attachment => this.addAttachment(attachment.type, attachment.value)
-            );
-
-        } catch (e) {
-            post = null;
-        }
-        
-        return new FormGroup({
-            title: new FormControl(post ? post.title : null, [Validators.required, Validators.minLength(5)]),
-            tags: new FormControl(post ? post.tags : null, [Validators.required]),
-            attachments: this.attachments
-        });
     }
 
     public addAttachment(type: AttachmentType, value?: any) {
@@ -83,24 +93,28 @@ export class PostFormRoute implements OnInit {
         this.form.controls.tags.setValue(tags);
         this.form.controls.tags.markAsDirty();
     }
-
+    
     public hasAuthorTag(): boolean {
         let tags: Tag[] = this.form.controls.tags.value || [];
 
         return tags.filter(
-            (item: Tag) => item.value == this.authorTag.value
-        ).length > 0;
+                (item: Tag) => item.value == this.authorTag.value
+            ).length > 0;
     }
-    
+
     public reset() {
-        this.form.reset();
         this.attachments.controls = [];
+        this.submitted = false;
+        this.form.reset();
     }
 
     public submit() {
+        this.submitted = true;
         let post: Post;
         console.log(this.form);
+        // console.log(this.form.value);
         localStorage.removeItem("new-post");
+        // this.form.controls['title'].getError("minlength").requiredLength
     }
 
     public handleEnterButton(e: KeyboardEvent) {
@@ -110,4 +124,17 @@ export class PostFormRoute implements OnInit {
             e.preventDefault()
         }
     }
-}
+
+    public requestAutocompleteItems = (text: string): Observable<Tag[]> => {
+        // @DOTO: Make rest service
+        return Observable
+            .of([
+                {value: text, display: text},
+                {value: text + " foo", display: text + " foo"},
+                {value: text + " bar", display: text + " bar"},
+                {value: text + " baz", display: text + " baz"}
+            ])
+        ;
+    };
+}   
+ 
