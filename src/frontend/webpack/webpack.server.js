15df9856f0593e1426ccb53b58fa6762d8a6a6db
hck <art.hacker@ya.ru>
2017-08-15 14:51:30 +0300

    update nginx config

--- src/frontend/webpack/webpack.server.js
+++ src/frontend/webpack/webpack.server.js
@@ -1,21 +1,16 @@
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const prodConfig = require('./webpack.prod.js');
 const commonConfig = require('./webpack.common.js');
 
-module.exports = [
-    // prodConfig, 
-    webpackMerge(commonConfig, {
-        devtool: 'source-map',
-    
-        entry: './app/server.ts',
-        output: {
-            filename: 'server.js'
-        },
-        target: 'node', // solves error: fs not found
-        plugins: [
-            new ExtractTextPlugin('[name].css')
-        ]
-    })
-];
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'source-map',
+    entry: './app/server.ts',
+    output: {
+        filename: 'server.js'
+    },
+    target: 'node', // solves error: fs not found
+    plugins: [
+        new ExtractTextPlugin('[name].css')
+    ]
+});
