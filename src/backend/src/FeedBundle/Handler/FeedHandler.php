91c57651b68c4e92dbfab890f0fa5e91405e5f2c
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2017-11-08 23:12:41 +0300

    FeedBundle fix feed limit posts

---
+++ src/backend/src/FeedBundle/Handler/FeedHandler.php
@@ -0,0 +1,14 @@
+<?php
+namespace FeedBundle\Handler;
+
+use FeedBundle\Criteria\Criteria;
+use FeedBundle\Criteria\FeedCriteria;
+use Symfony\Component\HttpFoundation\Request;
+
+class FeedHandler
+{
+    public function handleRequest(Request $request): FeedCriteria
+    {
+
+    }
+}
