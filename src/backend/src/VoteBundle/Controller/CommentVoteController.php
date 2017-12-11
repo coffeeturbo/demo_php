de25e1e3c2acbac5d3a2abaa2b57f92a37a381d0
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2017-12-11 17:41:01 +0300

    VoteBundle добавил роуты лайков для комментов

---
+++ src/backend/src/VoteBundle/Controller/CommentVoteController.php
@@ -0,0 +1,22 @@
+<?php
+namespace VoteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CommentVoteController extends Controller
+{
+    public function votePositiveAction($commentId)
+    {
+
+    }
+
+    public function voteNegativeAction($commentId)
+    {
+
+    }
+
+    public function voteRemoveAction($commentId)
+    {
+
+    }
+}
