3d768154dfed9af704dfd8e1a79fb1609dc8a65f
hck <art.hacker@ya.ru>
2018-01-16 15:45:00 +0300

    refresh comments after submit form handle diffrent attachments add recursive depth comments

--- src/frontend/modules/Comment/Component/CommentForm/index.ts
+++ src/frontend/modules/Comment/Component/CommentForm/index.ts
@@ -1,10 +1,16 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
-import {AttachmentType} from "../../../Attachment/Entity/Attachment";
+import {Attachment, AttachmentType} from "../../../Attachment/Entity/Attachment";
 import {Post} from "../../../Post/Entity/Post";
 import {Comment} from "../../Entity/Comment";
 import {CommentCreateRequest} from "../../Http/Request/CommentCreateRequest";
 import {CommentService} from "../../Service/CommentService";
+import {Observable} from "rxjs/Observable";
+import {AttachmentVideo} from "../../../Attachment/Entity/AttachmentVideo";
+import {AttachmentText} from "../../../Attachment/Entity/AttachmentText";
+import {AttachmentImage} from "../../../Attachment/Entity/AttachmentImage";
+import {AttachmentRESTService} from "../../../Attachment/Service/AttachmentRESTService";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
     selector: 'comment-form',
@@ -17,6 +23,7 @@ export class CommentFormComponent {
     @Input() focus: boolean = false;
     @Input() post: Post;
     @Input() parent: Comment;
+    @Output() onSubmit = new EventEmitter<Comment>();
     public AttachmentType = AttachmentType;
     public attachments = new FormArray([], Validators.required);
     public isLoading: boolean = false;
@@ -24,7 +31,11 @@ export class CommentFormComponent {
         attachments: this.attachments
     });
     
-    constructor(private commentService: CommentService) {}
+    constructor(
+        private route: ActivatedRoute,
+        private attachmentRest: AttachmentRESTService,
+        private commentService: CommentService
+    ) {}
 
     ngOnInit() {
         this.addAttachment(AttachmentType.text);
@@ -67,16 +78,52 @@ export class CommentFormComponent {
 
     submit() {
         this.isLoading = true;
-        let postCreateRequest: CommentCreateRequest = this.form.value; 
 
-        // Fck nelmio
-        postCreateRequest.attachments = JSON.stringify(postCreateRequest.attachments);
+        if(this.form.invalid) return;
+        
+        let commentCreateRequest: CommentCreateRequest = this.form.value;
 
-        this.commentService.create(postCreateRequest)
+        let attachmentsObservable: Observable<Attachment<AttachmentImage | AttachmentText | AttachmentVideo>>[] = [];
+
+        this.form.value.attachments.forEach(attachment => {
+            let attachmentObservable: Observable<Attachment<AttachmentImage | AttachmentText | AttachmentVideo>>;
+
+            switch(attachment.type) {
+                case AttachmentType.image:
+                    attachmentObservable = this.attachmentRest.uploadImage({image: attachment.value.image});
+                    break;
+                case AttachmentType.text:
+                    attachmentObservable = Observable.of(attachment);
+                    break;
+                case AttachmentType.video:
+                    attachmentObservable = this.attachmentRest.parseVideoLink({url: attachment.value});
+                    break;
+            }
+
+            attachmentsObservable.push(attachmentObservable);
+        });
+        
+        Observable
+            .combineLatest(attachmentsObservable)
+            .flatMap((attachments) => {
+                // Fck nelmio
+                commentCreateRequest.attachments = JSON.stringify(attachments);
+                return this.commentService.create(commentCreateRequest)
+            })
             .finally(() => this.isLoading = false)
-            .subscribe(() => {
+            .subscribe(comment => {
                 this.attachments.controls = [];
                 this.addAttachment(AttachmentType.text);
-            });
+                
+                if(this.parent) {
+                    this.parent.comments.push(comment);
+                    this.parent.comments_total++;
+                } else {
+                    this.route.snapshot.data.comments.push(comment)
+                }
+                
+                this.onSubmit.emit(this.parent);
+            })
+        ;
     }
 }
--- src/frontend/modules/Post/Route/PostRoute/index.ts
+++ src/frontend/modules/Post/Route/PostRoute/index.ts
@@ -1,7 +1,5 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Post} from "../../Entity/Post";
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {Comment} from "../../../Comment/Entity/Comment";
 import {ApplicationScrollService} from "../../../Application/Service/ApplicationScrollService";
 
 @Component({
@@ -9,29 +7,24 @@ import {ApplicationScrollService} from "../../../Application/Service/Application
     styleUrls: ['./style.shadow.scss']
 })
 
-export class PostRoute implements OnInit {
+export class PostRoute {
 
     @ViewChild('commentsEl') commentsEl: ElementRef;
 
-    public post: Post;
-    public comments: Comment[];
-
     constructor(
-        private route: ActivatedRoute, 
+        public route: ActivatedRoute, 
         private applicationScrollService: ApplicationScrollService
     ) {
-        this.route.fragment.subscribe(fragment => {
-            switch(fragment) {
-                case "comments" : 
-                    this.applicationScrollService.scrollTo(this.commentsEl.nativeElement.offsetTop, true);
-                break;
-                default : this.applicationScrollService.scrollTo(0, true);
-            }
-        });
-    }
-
-    ngOnInit() {
-        this.post = this.route.snapshot.data.post;
-        this.comments = this.route.snapshot.data.comments;
+        setTimeout(() => {
+            this.route.fragment.subscribe(fragment => {
+                switch (fragment) {
+                    case "comments" :
+                        this.applicationScrollService.scrollTo(this.commentsEl.nativeElement.offsetTop, true);
+                        break;
+                    default :
+                        this.applicationScrollService.scrollTo(0);
+                }
+            });
+        }, 0);
     }
 }
