6d7a10baedc25c499f07aca23238a586984527bd
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2018-04-20 15:45:08 +0300

    backend / PostBundle / Редактирование постов backend / AuthBundle / true false success fix

--- src/backend/src/PostBundle/Form/Handler/CreatePostDataHandler.php
+++ src/backend/src/PostBundle/Form/Handler/CreatePostDataHandler.php
@@ -2,10 +2,15 @@
 namespace PostBundle\Form\Handler;
 
 use AttachmentBundle\Service\AttachmentService;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\PersistentCollection;
 use PostBundle\Entity\Post;
+use PostBundle\Repository\PostRepository;
+use PostBundle\Service\PostService;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use TagBundle\Entity\Tag;
+use TagBundle\Response\SuccessTagsResponse;
 use TagBundle\Tag\TaggableEntityInterface;
 
 class CreatePostDataHandler
@@ -14,22 +19,42 @@ class CreatePostDataHandler
     private $maxTagsLimit;
     private $maxAttachmentsLimit;
     private $attachmentService;
+    private $postRepository;
 
     public function __construct(
         int $minTagsLimit,
         int $maxTagsLimit,
         int $maxAttachmentsLimit,
-        AttachmentService $attachmentService
+        AttachmentService $attachmentService,
+        PostRepository $postRepository
     ){
         $this->minTagsLimit = $minTagsLimit;
         $this->maxTagsLimit = $maxTagsLimit;
         $this->maxAttachmentsLimit = $maxAttachmentsLimit;
         $this->attachmentService = $attachmentService;
+        $this->postRepository = $postRepository;
     }
 
     public function handle(array $data): Post
     {
-        return $this->createFromData($data);
+        return isset($data['id'])? $this->updateFromData($data): $this->createFromData($data);
+    }
+
+    public function updateFromData(array $data): Post
+    {
+        /** @var Post $post */
+        $post = $this->postRepository->find($data['id']);
+
+        $post->setTitle($data['title']);
+        if($data['tags']) {
+            $this->setTagsFromJson($post, $data['tags']);
+        }
+
+//        if($data['attachments']) {
+//            $this->attachmentService->setAttachmentsFromJson($post, $data['attachments'], $this->maxAttachmentsLimit);
+//        }
+
+        return $post;
     }
 
 
@@ -44,7 +69,6 @@ class CreatePostDataHandler
 
         $newPost->setTitle($data['title']);
 
-        if(isset($data['id'])) $newPost->setId($data['id']);
 
         if($data['tags']) {
             $this->setTagsFromJson($newPost, $data['tags']);
@@ -61,6 +85,7 @@ class CreatePostDataHandler
     {
         $jsonTags = json_decode($jsonTagsString, true);
 
+
         if($this->minTagsLimit > count($jsonTags)) {
             throw new AccessDeniedHttpException(sprintf("min allowed tags: %s", $this->minTagsLimit));
         }
@@ -68,6 +93,13 @@ class CreatePostDataHandler
         if($this->maxTagsLimit < count($jsonTags)) {
             throw new AccessDeniedHttpException(sprintf("you have exceed tag limit: %s", $this->maxTagsLimit));
         }
+
+
+        //
+
+        $this->clearNotUsedTags($entity->getTags(), $jsonTags);
+
+
 
         foreach($jsonTags as $tagJson) {
             $tag = new Tag();
@@ -86,6 +118,19 @@ class CreatePostDataHandler
         }
 
         return $this;
+    }
+
+    public function clearNotUsedTags(PersistentCollection $entityTags, array $newTags)
+    {
+
+        print_r($entityTags->toArray());
+
+        // удаляем несовпадающие элементы
+
+        foreach($newTags as $newTag) {
+            
+        }
+
     }
 
 }
