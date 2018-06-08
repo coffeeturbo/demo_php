e0500c6d3493e326d0e8637ee7e2fe8988234842
hck <art.hacker@ya.ru>
2018-06-08 15:44:59 +0300

    add notice component in module

--- src/frontend/modules/Notice/NoticeModule.ts
+++ src/frontend/modules/Notice/NoticeModule.ts
@@ -3,13 +3,15 @@ import {NgModule} from "@angular/core";
 import {NoticeService} from "./Service/NoticeService";
 import {CommonModule} from "../Common/CommonModule";
 import {NotificationsComponent} from "./Component/Notifications";
+import {NoticeComponent} from "./Component/Notice";
 
 @NgModule({
     imports:[
         CommonModule,
     ],
     declarations: [
-        NotificationsComponent
+        NotificationsComponent,
+        NoticeComponent
     ],
     providers: [
         NoticeService
