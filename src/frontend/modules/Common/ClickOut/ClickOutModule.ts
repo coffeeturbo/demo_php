fd9a15cca002db52066bad1a87c77c41faa8b2de
hck <art.hacker@ya.ru>
2018-06-08 15:38:42 +0300

    add misssing files

---
+++ src/frontend/modules/Common/ClickOut/ClickOutModule.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {ClickOutDirective} from "./Directive/ClickOutDirective";
+
+@NgModule({
+    imports:[
+        
+    ],
+    declarations: [
+        ClickOutDirective
+    ],
+    exports: [
+        ClickOutDirective
+    ]
+})
+export class ClickOutModule {} 
