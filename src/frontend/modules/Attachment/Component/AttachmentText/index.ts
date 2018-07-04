8df589a276b97e3e1d9954d79a83db31f27ae052
hck <art.hacker@ya.ru>
2018-07-04 14:16:11 +0300

    // @TODO implement target="blank" !

--- src/frontend/modules/Attachment/Component/AttachmentText/index.ts
+++ src/frontend/modules/Attachment/Component/AttachmentText/index.ts
@@ -11,5 +11,11 @@ export class AttachmentTextComponent implements AfterViewInit {
 
     ngAfterViewInit() {
         this.el.nativeElement.innerHTML = this.text.replace(/\n/g, "<br/>");
+
+        for (let a of this.el.nativeElement.getElementsByTagName('a')) {
+            if( location.host != a.host ) {
+                a.setAttribute('target', '_blank');
+            }
+        }
     }
 }
