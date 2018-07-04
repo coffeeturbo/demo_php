bf4c858c29983e23211342353c4e7e065aafbc1b
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2018-07-04 17:13:00 +0300

    backend / AuthBundle / Переименовал методы более логично

--- src/backend/src/AuthBundle/Controller/ConfirmMailController.php
+++ src/backend/src/AuthBundle/Controller/ConfirmMailController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
-class SendMailController extends Controller
+class ConfirmMailController extends Controller
 {
 
     /**
