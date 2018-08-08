2753fad9e772de16ea310e7ee752f6c9a84de699
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2018-08-08 16:08:36 +0300

    VotedFeed добавил обработку ошибки profileId

--- src/backend/src/FeedBundle/Strategy/VotedFeedStrategy.php
+++ src/backend/src/FeedBundle/Strategy/VotedFeedStrategy.php
@@ -1,12 +1,15 @@
 <?php
 namespace FeedBundle\Strategy;
 
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use VoteBundle\Entity\Vote;
 
 class VotedFeedStrategy extends FeedStrategy
 {
     function getPosts()
     {
+        if(!$this->feedCriteria->getProfileId())throw new BadRequestHttpException('profile id required');
+
         // получаем войты
         $voteRepositrory = $this->getPostRepository()->getEntityManager()->getRepository(Vote::class);
         // получаем посты из войтов
