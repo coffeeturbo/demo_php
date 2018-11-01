e5324c7ea62a01dc9a5bc88caa4396dd5c3ec4b5
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2018-11-01 22:40:57 +0300

    PostBundle - seo extrafield

--- src/backend/src/PostBundle/Formatter/PostFormatter.php
+++ src/backend/src/PostBundle/Formatter/PostFormatter.php
@@ -29,9 +29,11 @@ class PostFormatter extends Formatter
                     'positive' => $this->resource->getVotesPositive(),
                     'negative' => $this->resource->getVotesNegative()
                 ],
-                'seo' => is_null($this->resource->getSeo())
-                    ? null
-                    : json_decode($this->resource->getSeo())
+
+
+                'seo' => $this->resource->getSeo()
+                    ? json_decode($this->resource->getSeo())
+                    : null
             ];
         }
     }
